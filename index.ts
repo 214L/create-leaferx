b176a97fb230d3008fd6788134fba7b4407968df
214L <Aierc_W@hotmail.com>
2024-08-25 16:54:41 +0800

    feat : handle directory
    @description : handle empty or make dir

--- index.ts
+++ index.ts
@@ -6,31 +6,40 @@ import process from 'node:process'
 import { parseArgs } from 'node:util'
 import prompts from 'prompts'
 import * as banners from './utils/banners'
-import { getLeaferVersion, getPrompt } from './utils/index'
+import {
+  getLeaferVersion,
+  getPrompt,
+  canSkipOverwriteOption,
+  emptyDirectory
+} from './utils/index'
+import { red, green, bold } from 'kolorist'
+import ora from 'ora'
 
+const spinner = ora(`Loading ${chalk.red('unicorns')}`).start()
 async function init() {
-  console.log(0)
+  console.log()
   console.log(
     process.stdout.isTTY && process.stdout.getColorDepth() > 8
       ? banners.gradientBanner
       : banners.defaultBanner
   )
   console.log()
-  let res = await getLeaferVersion()
-  console.log(res)
-
+  let leaferVersion = await getLeaferVersion()
 
   const promptMessage = getPrompt()
-  
+
   const args = process.argv.slice(2)
 
- 
-  const { values: argv ,positionals} = parseArgs({
+  const { values: argv, positionals } = parseArgs({
     args,
     strict: false
   })
   let targetDir = positionals[0]
-  let result= {}
+  let result: {
+    projectName?: string
+    shouldOverwrite?: boolean
+    packageName?: string
+  } = {}
 
   try {
     // Prompts:
@@ -42,31 +51,69 @@ async function init() {
         name: 'projectName',
         type: 'text',
         message: promptMessage.projectName.message,
-        initial: 'leafer-x-' ,
-        onState: (state) => (targetDir = String(state.value).trim() || 'leafer-x-')
+        initial: 'leafer-x-',
+        onState: state =>
+          (targetDir = String(state.value).trim() || 'leafer-x-')
+      },
+      {
+        name: 'shouldOverwrite',
+        type: () => (canSkipOverwriteOption(targetDir) ? null : 'toggle'),
+        message: () => {
+          const dirForPrompt =
+            targetDir === '.'
+              ? promptMessage.shouldOverwrite.dirForPrompts.current
+              : `${promptMessage.shouldOverwrite.dirForPrompts.target} "${targetDir}"`
+
+          return `${dirForPrompt} ${promptMessage.shouldOverwrite.message}`
+        },
+        initial: true,
+        active: promptMessage.defaultToggleOptions.active,
+        inactive: promptMessage.defaultToggleOptions.inactive
+      },
+      {
+        name: 'overwriteChecker',
+        type: (prev, values) => {
+          if (values.shouldOverwrite === false) {
+            throw new Error(
+              red('✖') + ` ${promptMessage.errors.operationCancelled}`
+            )
+          }
+          return null
+        }
       },
       {
         type: 'multiselect',
         name: 'supportPlatforms',
         message: promptMessage.supportPlatforms.message,
         choices: [
-            { title: 'web', value: 'web' , selected: true },
-            { title: 'worker', value: 'worker', selected: true  },
-            { title: 'node', value: 'node' , selected: true },
-            { title: 'miniapp', value: 'miniapp' , selected: true },
+          { title: 'web', value: 'web', selected: true },
+          { title: 'worker', value: 'worker', selected: true },
+          { title: 'node', value: 'node', selected: true },
+          { title: 'miniapp', value: 'miniapp', selected: true }
         ],
         hint: promptMessage.supportPlatforms.hint,
-        instructions: false, 
-        min: 1, 
+        instructions: false,
+        min: 1,
         max: 4
       }
     ])
-    console.log(result);
-    console.log(targetDir);
+    let { shouldOverwrite } = result
+
+    const cwd = process.cwd()
+    const root = path.join(cwd, targetDir)
+
     
+    if (fs.existsSync(root) && shouldOverwrite) {
+      const spinner = ora('emptying dir...').start()
+      emptyDirectory(root)
+      spinner.succeed('emptying dir succeed.')
+    } else if (!fs.existsSync(root)) {
+      fs.mkdirSync(root)
+    }
+
     console.log('finish')
   } catch (cancelled) {
-    console.log('cancelled')
+    console.log('cancelled', cancelled)
     process.exit(1)
   }
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export * from './banners'
 export * from './getUserInfo'
+export * from './handleFile'
